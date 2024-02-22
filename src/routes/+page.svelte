6c85f301cc10e0109a814ebb536e9e6235cb09b9
Pokatow <0xpokatow@gmail.com>
2024-02-22 22:16:20 +0700

    latest

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
 <script lang="ts">
+	import Seo from '$lib/components/Seo.svelte'
 </script>
+
+<Seo title={``} description={``} />
+<section>
+	<!-- code here -->
+</section>
+
+<style></style>
--- src/routes/api/hello-world/+server.ts
+++
@@ -1,8 +0,0 @@
-import type { RequestHandler } from './$types'
-
-export const GET: RequestHandler =
-	async (event) => {
-		return new Response(
-			'hello-world'
-		)
-	}
